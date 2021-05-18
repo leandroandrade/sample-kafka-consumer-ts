f917dfda6d2de9a0e756d1ca588fafbac1f61236
leandroandrade <leandro.andrade2588@gmail.com>
2021-05-18 09:51:44 -0300

    refactor

---
+++ src/configurations/shutdown/index.ts
@@ -0,0 +1,14 @@
+import { Consumer } from 'kafkajs';
+
+const handle = (signal: string, consumer: Consumer) => {
+    console.log(`[${signal}] Disconnecting Kafka broker...`);
+    consumer.disconnect()
+        .then(() => {
+            console.log('Consumer disconnected successful!');
+            process.exit(0);
+        });
+};
+
+export const exit = (consumer: Consumer): void => {
+    ['SIGTERM', 'SIGINT'].forEach((signal) => process.on(signal, () => handle(signal, consumer)));
+};
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import env from './env';
 import { KafkaHelper } from './configurations/kafka';
+import { exit } from './configurations/shutdown';
 
 Promise.all([KafkaHelper.connect(env)])
     .then(async ([consumer]) => {
         console.log('Consumer connected on brokers!');
 
         (await import('./configurations/app')).default({ env, consumer });
+        exit(consumer);
     })
     .catch((err) => console.error(err));
