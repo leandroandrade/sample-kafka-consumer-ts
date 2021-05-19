25a831985d298f895dda38db56c940fc66aa0997
leandroandrade <leandro.andrade2588@gmail.com>
2021-05-19 13:30:05 -0300

    refactor

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,13 @@ import { ConsumerHelper } from './configurations/consumer';
 
 Promise.all([ConsumerHelper.connect(env)])
     .then(async () => {
-        const app = (await import('./configurations/app')).default;
-        app.listen(3000, () => console.log('Server running at port 3000'));
-
         const consumer = (await import('./domain/consumers')).default;
         consumer.listen()
-            .then(() => console.log('Consumer ready and listening...'))
+            .then(async () => {
+                console.log('Consumer ready and listening...');
+                const app = (await import('./configurations/app')).default;
+                app.listen(3000, () => console.log('Server running at port 3000'));
+            })
             .catch((err) => console.error('Consumer error:', err));
     })
     .catch((err) => console.error(err));
